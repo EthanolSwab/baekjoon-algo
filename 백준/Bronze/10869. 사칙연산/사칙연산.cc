9ea110819aea8f6d21806c0f1536bd71fe9d50dd
EthanolSwab <110582402+EthanolSwab@users.noreply.github.com>
2024-04-11 17:13:01 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main (void){
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+}
