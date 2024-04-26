cd610f4ddba9cf4ef9f696899e9ed9000eb4e604
EthanolSwab <110582402+EthanolSwab@users.noreply.github.com>
2024-04-26 14:57:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 113.45 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr){
+    vector<int> answer;
+    int check = -1;
+    
+    for(int i = 0; i<arr.size(); i++){
+        if(check != arr[i]){
+            answer.push_back(arr[i]);
+            check = arr[i];
+        }
+    }
+    return answer;
+}
