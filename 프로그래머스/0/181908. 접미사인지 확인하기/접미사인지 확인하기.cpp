a0bf4d3324c46c239fc5b4a045a5ca343313d781
EthanolSwab <110582402+EthanolSwab@users.noreply.github.com>
2024-05-29 23:20:12 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    
+    if(is_suffix.size() > my_string.size()) return 0;
+    
+    if(my_string.substr(my_string.size()-is_suffix.size()) == is_suffix)
+        return 1;
+    return 0;
+
+}
